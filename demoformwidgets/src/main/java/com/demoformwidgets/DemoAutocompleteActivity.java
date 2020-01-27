0e61b83a67d30ef4915a8dbbb8578cef8c55f6ae
DTA_KO <dtako.developer@gmail.com>
2020-01-27 12:04:15 +0700

    change some thing

---
+++ demoformwidgets/src/main/java/com/demoformwidgets/DemoAutocompleteActivity.java
@@ -0,0 +1,14 @@
+package com.demoformwidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoAutocompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_autocomplete);
+    }
+}
---
+++ demographic/src/main/java/com/demographic/DemoShowBitmapActivity.java
@@ -0,0 +1,14 @@
+package com.demographic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoShowBitmapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_show_bitmap);
+    }
+}
